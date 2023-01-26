63974a06d6abe9bd3dc78c1baacffb00a10f8e15
mohamed khaled <mookhaled888@gmail.com>
2023-01-27 00:30:49 +0200

    Add Lable Date Of Birth

--- Employee1/Employee.Designer.cs
+++ Employee1/Employee.Designer.cs
@@ -159,11 +159,11 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Teal;
-            this.label4.Location = new System.Drawing.Point(26, 539);
+            this.label4.Location = new System.Drawing.Point(26, 528);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(141, 25);
+            this.label4.Size = new System.Drawing.Size(123, 25);
             this.label4.TabIndex = 98;
-            this.label4.Text = "Salary Amount";
+            this.label4.Text = "Date Of Birth";
             // 
             // label13
             // 

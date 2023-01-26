58aeb8f03c4012a810baf41894f0d88a20b3aab1
mohamed khaled <mookhaled888@gmail.com>
2023-01-27 00:15:05 +0200

    font 12pt

--- Employee1/Employee.Designer.cs
+++ Employee1/Employee.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Employee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 703);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Employee";
             this.Text = "Employee";
+            this.ResumeLayout(false);
+
         }
 
         #endregion

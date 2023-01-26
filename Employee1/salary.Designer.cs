b4be0d740c346b6aa18a7ba3d77ea65ee9bac03d
mohamed khaled <mookhaled888@gmail.com>
2023-01-27 01:17:56 +0200

    Add Panel in top

--- Employee1/salary.Designer.cs
+++ Employee1/salary.Designer.cs
@@ -292,7 +292,7 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1395, 220);
+            this.panel1.Size = new System.Drawing.Size(1395, 201);
             this.panel1.TabIndex = 83;
             // 
             // label1

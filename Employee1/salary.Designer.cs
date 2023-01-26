2de2aef2e1136e6c950843037146d391ab0361a9
mohamed khaled <mookhaled888@gmail.com>
2023-01-27 01:15:04 +0200

    Add lable to Salary Amount

--- Employee1/salary.Designer.cs
+++ Employee1/salary.Designer.cs
@@ -185,7 +185,7 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Teal;
-            this.label4.Location = new System.Drawing.Point(26, 503);
+            this.label4.Location = new System.Drawing.Point(21, 503);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(141, 25);
             this.label4.TabIndex = 101;

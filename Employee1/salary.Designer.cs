62c64d2d417086711bde1a407b46475458be8fae
mohamed khaled <mookhaled888@gmail.com>
2023-01-27 01:11:18 +0200

    Add PictureBox To Logout

--- Employee1/salary.Designer.cs
+++ Employee1/salary.Designer.cs
@@ -270,7 +270,7 @@
             // pictureBox5
             // 
             this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
-            this.pictureBox5.Location = new System.Drawing.Point(895, 198);
+            this.pictureBox5.Location = new System.Drawing.Point(895, 226);
             this.pictureBox5.Name = "pictureBox5";
             this.pictureBox5.Size = new System.Drawing.Size(33, 40);
             this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;

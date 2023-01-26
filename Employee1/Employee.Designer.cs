7c0cf018974ac83e4b424599f9cd45b58467b9f2
mohamed khaled <mookhaled888@gmail.com>
2023-01-27 00:15:29 +0200

    back color white

--- Employee1/Employee.Designer.cs
+++ Employee1/Employee.Designer.cs
@@ -34,6 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1200, 703);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);

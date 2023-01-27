eb7e5b0ae5e06531e580a827845866808bb4329e
mohamed khaled <mookhaled888@gmail.com>
2023-01-27 14:19:55 +0200

    Employee Coding

--- Employee1/Employees.Designer.cs
+++ Employee1/Employees.Designer.cs
@@ -292,6 +292,7 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(221, 30);
             this.textBox2.TabIndex = 108;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // comboBox1
             // 

0b37bf00278584f87f36c246b4de0d12f306d7d8
mohamed khaled <mookhaled888@gmail.com>
2023-01-27 14:26:37 +0200

    add string Name = EmpNameTb.Text;

--- Employee1/Employees.cs
+++ Employee1/Employees.cs
@@ -120,7 +120,7 @@ namespace EmployeeMgmt1
                 }
                 else 
                 {
-                    string Name = EmpNameTb.Text;
+                    string Name = EmpNameTb.Text; 
                     string Gender = GenCb.SelectedItem.ToString();
                     int Dep = Convert.ToInt32(DepCb.SelectedValue.ToString());
                     string DOB = DOBTb.Value.ToString();

56dda7eddb8b08c7264e4ab939950893f9713247
mohamed khaled <mookhaled888@gmail.com>
2023-01-27 13:35:03 +0200

    add Con.SetData(Query);

--- Employee1/Departments.cs
+++ Employee1/Departments.cs
@@ -39,7 +39,7 @@ namespace EmployeeMgmt1
                     string Dep = DepNameTb.Text; 
                     string Query = "insert into DepartmentTb1 values('{0}')"; 
                     Query = string.Format(Query, DepNameTb.Text); 
-                    Con.SetData(Query);
+                    Con.SetData(Query); 
                     ShowDepartments();
                     MessageBox.Show("Department Added!!!");
                     DepNameTb.Text = "";

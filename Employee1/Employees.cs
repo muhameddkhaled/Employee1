80794046aa167f493c09bd721673dbf8d849264a
mohamed khaled <mookhaled888@gmail.com>
2023-01-27 14:24:49 +0200

    add Con.SetData(Query);

--- Employee1/Employees.cs
+++ Employee1/Employees.cs
@@ -95,7 +95,7 @@ namespace EmployeeMgmt1
                     int Salary = Convert.ToInt32(DailySalTb.Text); 
                     string Query = "insert into EmployeeTb1 values('{0}','{1}',{2},'{3}','{4}',{5})";
                     Query = string.Format(Query, Name, Gender, Dep, DOB, JDate, Salary); 
-                    Con.SetData(Query);
+                    Con.SetData(Query); 
                     ShowEmp();
                     MessageBox.Show("Emoloyee Updated!!!");
                     EmpNameTb.Text = "";

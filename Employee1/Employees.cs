0003232e58f39c933eafa72379214fc69b232789
mohamed khaled <mookhaled888@gmail.com>
2023-01-27 14:27:03 +0200

    add string DOB = DOBTb.Value.ToString();

--- Employee1/Employees.cs
+++ Employee1/Employees.cs
@@ -123,7 +123,7 @@ namespace EmployeeMgmt1
                     string Name = EmpNameTb.Text; 
                     string Gender = GenCb.SelectedItem.ToString();  
                     int Dep = Convert.ToInt32(DepCb.SelectedValue.ToString()); 
-                    string DOB = DOBTb.Value.ToString();
+                    string DOB = DOBTb.Value.ToString(); 
                     string JDate = JDateTb.Value.ToString();
                     int Salary = Convert.ToInt32(DailySalTb.Text);
                     string Query = "Delete from EmployeeTb1 where Empid= {0}";

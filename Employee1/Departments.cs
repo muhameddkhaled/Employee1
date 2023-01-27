3eb54034d133990804f58abd10b78148c36896a1
mohamed khaled <mookhaled888@gmail.com>
2023-01-27 13:33:31 +0200

    add try

--- Employee1/Departments.cs
+++ Employee1/Departments.cs
@@ -29,7 +29,7 @@ namespace EmployeeMgmt1
         private void AddBtn_Click(object sender, EventArgs e) 
         {
             try
-            {
+            { 
                 if (DepNameTb.Text == "")
                 {
                     MessageBox.Show("missing data!!!");

58f44c66555ead86da2fb6df0b59e296e6e062ac
mohamed khaled <mookhaled888@gmail.com>
2023-01-27 13:40:31 +0200

    add  if (DepNameTb.Text == "")

--- Employee1/Departments.cs
+++ Employee1/Departments.cs
@@ -93,7 +93,7 @@ namespace EmployeeMgmt1
         {
             try 
             {
-                if (DepNameTb.Text == "")
+                if (DepNameTb.Text == "") 
                 {
                     MessageBox.Show("missing data!!!");
                 }

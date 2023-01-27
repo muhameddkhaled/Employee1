9827a49551405de41b7260b8d0419e9a1332386e
mohamed khaled <mookhaled888@gmail.com>
2023-01-27 13:40:24 +0200

    add try

--- Employee1/Departments.cs
+++ Employee1/Departments.cs
@@ -91,7 +91,7 @@ namespace EmployeeMgmt1
 
         private void DeleteBtn_Click(object sender, EventArgs e) 
         {
-            try
+            try 
             {
                 if (DepNameTb.Text == "")
                 {

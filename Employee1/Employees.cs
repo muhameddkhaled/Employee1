f882c5503207e64e97fee0bb5a214b07dd00bbc5
mohamed khaled <mookhaled888@gmail.com>
2023-01-27 14:27:34 +0200

    add Query = string.Format(Query, key);

--- Employee1/Employees.cs
+++ Employee1/Employees.cs
@@ -127,7 +127,7 @@ namespace EmployeeMgmt1
                     string JDate = JDateTb.Value.ToString(); 
                     int Salary = Convert.ToInt32(DailySalTb.Text); 
                     string Query = "Delete from EmployeeTb1 where Empid= {0}"; 
-                    Query = string.Format(Query, key);
+                    Query = string.Format(Query, key); 
                     Con.SetData(Query);
                     ShowEmp();
                     MessageBox.Show("Emoloyee Deleted!!!");

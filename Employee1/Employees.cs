9829a413565be4e3138b9413562232cd217d6aa0
mohamed khaled <mookhaled888@gmail.com>
2023-01-27 14:22:24 +0200

    add DepCb.ValueMember = Con.GetData(Query).Columns["Depid"].ToString();

--- Employee1/Employees.cs
+++ Employee1/Employees.cs
@@ -73,7 +73,7 @@ namespace EmployeeMgmt1
         { 
             string Query = "Select * from DepartmentTb1";
             DepCb.DisplayMember = Con.GetData(Query).Columns["Depname"].ToString();
-            DepCb.ValueMember = Con.GetData(Query).Columns["Depid"].ToString(); 
+            DepCb.ValueMember = Con.GetData(Query).Columns["Depid"].ToString();  
             DepCb.DataSource = Con.GetData(Query);
 
         }

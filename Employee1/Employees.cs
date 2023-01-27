e2c3df9b63f912603a38fb19b2735a02d965115c
mohamed khaled <mookhaled888@gmail.com>
2023-01-27 14:20:27 +0200

    public Employees()

--- Employee1/Employees.cs
+++ Employee1/Employees.cs
@@ -14,7 +14,7 @@ namespace EmployeeMgmt1
     public partial class Employees : Form
     {
         Functions Con; 
-        public Employees()
+        public Employees() 
         {
             InitializeComponent();
             Con = new Functions();

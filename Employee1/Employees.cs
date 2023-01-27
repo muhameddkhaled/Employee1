e5db4661565b7363c4afed16ecf6b827658527f3
mohamed khaled <mookhaled888@gmail.com>
2023-01-27 14:22:44 +0200

    add DepCb.DataSource = Con.GetData(Query);

--- Employee1/Employees.cs
+++ Employee1/Employees.cs
@@ -74,7 +74,7 @@ namespace EmployeeMgmt1
             string Query = "Select * from DepartmentTb1";
             DepCb.DisplayMember = Con.GetData(Query).Columns["Depname"].ToString();
             DepCb.ValueMember = Con.GetData(Query).Columns["Depid"].ToString();  
-            DepCb.DataSource = Con.GetData(Query);
+            DepCb.DataSource = Con.GetData(Query); 
 
         }
         private void AddBtn_Click(object sender, EventArgs e)

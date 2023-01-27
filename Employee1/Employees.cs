2bcae52395308743d42f69cd9f204f8466787ff1
mohamed khaled <mookhaled888@gmail.com>
2023-01-27 14:21:24 +0200

    add string Query = "Select * from EmployeeTb1";

--- Employee1/Employees.cs
+++ Employee1/Employees.cs
@@ -25,7 +25,7 @@ namespace EmployeeMgmt1
         {
             try 
             {
-                string Query = "Select * from EmployeeTb1";
+                string Query = "Select * from EmployeeTb1"; 
                 EmployeeList.DataSource = Con.GetData(Query);
             }
             catch (Exception)

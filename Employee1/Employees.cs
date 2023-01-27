853483445732a4ae49d92ca834bb7fc48452b68b
mohamed khaled <mookhaled888@gmail.com>
2023-01-27 14:24:03 +0200

    add string JDate = JDateTb.Value.ToString();

--- Employee1/Employees.cs
+++ Employee1/Employees.cs
@@ -91,7 +91,7 @@ namespace EmployeeMgmt1
                     string Gender = GenCb.SelectedItem.ToString(); 
                     int Dep = Convert.ToInt32(DepCb.SelectedValue.ToString()); 
                     string DOB = DOBTb.Value.ToString(); 
-                    string JDate = JDateTb.Value.ToString();
+                    string JDate = JDateTb.Value.ToString(); 
                     int Salary = Convert.ToInt32(DailySalTb.Text);
                     string Query = "insert into EmployeeTb1 values('{0}','{1}',{2},'{3}','{4}',{5})";
                     Query = string.Format(Query, Name, Gender, Dep, DOB, JDate, Salary);

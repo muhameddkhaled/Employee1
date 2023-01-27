be939f221780e87cf44535e58cc0d074433feec8
mohamed khaled <mookhaled888@gmail.com>
2023-01-27 14:22:03 +0200

    add string Query = "Select * from DepartmentTb1";

--- Employee1/Employees.cs
+++ Employee1/Employees.cs
@@ -70,7 +70,7 @@ namespace EmployeeMgmt1
         }
 
         private void GetDepartment()
-        {
+        { 
             string Query = "Select * from DepartmentTb1";
             DepCb.DisplayMember = Con.GetData(Query).Columns["Depname"].ToString();
             DepCb.ValueMember = Con.GetData(Query).Columns["Depid"].ToString();

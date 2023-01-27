bee4907fe43c6846152b44ce813aef3479cf037f
mohamed khaled <mookhaled888@gmail.com>
2023-01-27 13:39:53 +0200

    add MessageBox.Show(Ex.Message);

--- Employee1/Departments.cs
+++ Employee1/Departments.cs
@@ -85,7 +85,7 @@ namespace EmployeeMgmt1
             }
             catch (Exception Ex) 
             {
-                MessageBox.Show(Ex.Message);
+                MessageBox.Show(Ex.Message); 
             }
         }
 

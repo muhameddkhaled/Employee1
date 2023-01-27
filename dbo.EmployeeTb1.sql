06392dbfcc6004e1389d5699f90d79e9aa54a72c
muhameddkhaled <123648495+muhameddkhaled@users.noreply.github.com>
2023-01-27 18:51:17 +0200

    Add files via upload

---
+++ dbo.EmployeeTb1.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[EmployeeTb1] (
+    [EmpId]    INT          IDENTITY (1, 1) NOT NULL,
+    [EmpName]  VARCHAR (50) NOT NULL,
+    [EmpGen]   VARCHAR (10) NOT NULL,
+    [EmpDep]   INT          NOT NULL,
+    [EmpDOB]   INT          NOT NULL,
+    [EmpJDate] DATE         NOT NULL,
+    [Empsal]   INT          NOT NULL,
+    PRIMARY KEY CLUSTERED ([EmpId] ASC),
+    CONSTRAINT [FK1] FOREIGN KEY ([EmpDep]) REFERENCES [dbo].[DepartmentTb1] ([Depid])
+);
+
---
+++ dbo.SalaryTb1.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[SalaryTb1] (
+    [SCode]      INT           IDENTITY (1, 1) NOT NULL,
+    [Employee]   INT           NOT NULL,
+    [Attendance] INT           NOT NULL,
+    [Period]     VARCHAR (100) NOT NULL,
+    [Amount]     INT           NOT NULL,
+    [PayDate]    DATE          NOT NULL,
+    PRIMARY KEY CLUSTERED ([SCode] ASC),
+    CONSTRAINT [FK3] FOREIGN KEY ([Employee]) REFERENCES [dbo].[EmployeeTb1] ([EmpId])
+);
+

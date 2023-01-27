d5e731645d5b30ab6950c24ab863b04923270e43
mohamed khaled <mookhaled888@gmail.com>
2023-01-27 13:40:40 +0200

    add MessageBox.Show("missing data!!!");

--- Employee1/Departments.cs
+++ Employee1/Departments.cs
@@ -95,7 +95,7 @@ namespace EmployeeMgmt1
             {
                 if (DepNameTb.Text == "") 
                 {
-                    MessageBox.Show("missing data!!!");
+                    MessageBox.Show("missing data!!!"); 
                 }
                 else
                 {
